b5b02d08f09cfff1de792dc6fb7417cb85cd81f2
ShahzaibShakoor <shahzaib.shakoor@logicps.com>
2024-08-08 19:24:52 +0500

    gap fixes

--- src/components/descSection/DescSection.tsx
+++ src/components/descSection/DescSection.tsx
@@ -1,12 +1,12 @@
 export default function DescSection() {
   return (
-    <section className="grid grid-cols-[4fr_3fr] px-[16%] pt-24 pb-56 desc-background m-0 gap-10">
-      <div className="flex flex-col gap-10 self-center p-9">
-        <h1 className="font-arealMT text-5xl">
-          Look, up in the <span className="text-[#34AAFF]">sky</span>,<br/>it’s your
+    <section className="grid grid-cols-[4fr_3fr] px-[16%] pt-24 pb-56 desc-background m-0">
+      <div className="flex flex-col gap-10 self-center">
+        <h1 className="font-arealMT text-5xl w-[28rem]">
+          Look, up in the <span className="text-[#34AAFF]">sky</span>, it’s your
           dinner
         </h1>
-        <p className="font-openSans text-lg">
+        <p className="font-openSans text-lg w-[40rem]">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. It is a long
           established fact that a reader will be distracted by the readable
--- src/components/featureCard/FeatureCard.tsx
+++ src/components/featureCard/FeatureCard.tsx
@@ -8,7 +8,7 @@ export default function FeatureCard({
   description: string;
 }) {
   return (
-    <div className="flex flex-col items-center w-full h-full gap-10 shadow p-6 rounded-xl hover:-translate-y-7 hover:shadow-2xl transition-all duration-[400ms]">
+    <div className="flex flex-col items-center w-96 h-full gap-10 shadow p-6 rounded-xl hover:-translate-y-7 hover:shadow-2xl transition-all duration-[400ms]">
       <img src={image} alt="" className="mix-blend-darken h-24" />
       <h1 className="text-center font-arealMT text-4xl">{title}</h1>
       <p className="text-center font-openSans text-base">{description}</p>
